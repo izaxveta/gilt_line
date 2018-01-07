98163e1f8ae2c25f46d072a1256b15f2ed181fef
izaxveta <izavetha@gmail.com>
2018-01-06 18:01:15 -0700

    Opened route for newly registered users visiting their dasboard.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/dashboard', to: 'dashboard#index'
   resources :users, only: [:create]
 end
