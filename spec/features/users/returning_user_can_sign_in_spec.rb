d428abcc67bfc8d5c4fd095223af444e63c8e14e
izaxveta <izavetha@gmail.com>
2018-01-06 18:14:48 -0700

    Created spec for returning users signing in, test passing.

---
+++ spec/features/users/returning_user_can_sign_in_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature 'As a returning unauthenticated user' do
+  scenario 'I can navigate to the sign in page and sign in' do
+    user = User.create(first_name: 'Lourdes',
+                       last_name: 'Laurentum',
+                       email: 'lourdes@mail.com',
+                       password: 'password')
+
+    visit '/'
+    click_on 'SIGN IN'
+
+    expect(current_path).to eq('/login')
+
+    fill_in 'session[email]', with: 'lourdes@mail.com'
+    fill_in 'session[password]', with: 'password'
+
+    click_on 'GO'
+
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content('Lourdes Laurentum')
+  end
+end
