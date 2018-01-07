a1a9997d15d4ee2a0745187a6fa80f9f9e459749
izaxveta <izavetha@gmail.com>
2018-01-06 23:08:51 -0700

    Created UserBrand model

---
+++ app/models/user_brand.rb
@@ -0,0 +1,5 @@
+  validates_uniqueness_of :user_id, scope: :brand_id
+  def self.set_favorite_brands(brand_ids, user)
+    user.brands.delete_all
+    brand_ids.each { |id| user.brands << Brand.find(id) if id != '' }
+  end
