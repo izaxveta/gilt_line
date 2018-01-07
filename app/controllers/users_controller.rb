3d6cb895d1ba8f37ab22d566811592f74f9f5868
izaxveta <izavetha@gmail.com>
2018-01-06 23:04:27 -0700

    Updated user_params to reflect updates to users table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,12 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password)
+      params.require(:user).permit(:first_name,
+                                   :last_name,
+                                   :email,
+                                   :password,
+                                   :birthday,
+                                   :nationality,
+                                   :occupation)
     end
 end
