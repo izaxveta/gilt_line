2729c82cf4e9cd2101968e7c1affcfb997f9bff4
izaxveta <izavetha@gmail.com>
2018-01-06 18:16:22 -0700

    Built out sessions#create functionality to authenticate a user.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'Sign in successful!'
+      redirect_to dashboard_path(id: user.id)
+    else
+      render :new
+    end
+  end
+end
