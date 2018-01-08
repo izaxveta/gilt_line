ff7dbf9c2d7de11ba7644402f1a5f174c7ce8db7
izaxveta <izavetha@gmail.com>
2018-01-08 15:58:54 -0700

    Initialized and created connection with Yelp API for venue service PORO

---
+++ app/services/venue_service.rb
@@ -0,0 +1,19 @@
+class VenueService
+  def get_venues_by_location(filter)
+    get_json("search?term=restaruants&location=10036&price=3,4&location=#{filter}")
+  end
+
+  private
+
+    def get_json(url)
+      response = connection.get(url)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
+    def connection
+      @connection ||= Faraday.new(url: "https://api.yelp.com/v3/businesses/") do |faraday|
+        faraday.headers['Authorization'] = "Bearer #{ENV['YELP_API_KEY']}"
+        faraday.adapter Faraday.default_adapter
+      end
+    end
+end
