9cfb23f2668e09a14c3b856974d0cf4579d7be49
izaxveta <izavetha@gmail.com>
2018-01-08 20:08:47 -0700

    Reorganzied and grouped routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-
-  get 'admin/dashboard', to: 'admin/dashboard#index'
-  get '/dashboard',      to: 'dashboard#index'
   get '/login',          to: 'sessions#new'
   post '/login',         to: 'sessions#create'
   delete '/login',       to: 'sessions#destroy', as: 'logout'
+
+  get 'admin/dashboard', to: 'admin/dashboard#index'
   get 'admin/settings',  to: 'admin/dashboard#show', as: 'settings'
 
   get '/admin/fashion-brands', to: 'admin/fashion_brands#index', as: 'fashion_brands'
   get '/admin/fashion-brands/new', to: 'admin/fashion_brands#new', as: 'new_fashion_brand'
   post '/admin/fashion-brands', to: 'admin/fashion_brands#create'
+
+  get '/dashboard',      to: 'dashboard#index'
 
   get '/profile',        to: 'users#show',       as: 'profile'
   get '/profile/edit',   to: 'users#edit',       as: 'edit_profile'
