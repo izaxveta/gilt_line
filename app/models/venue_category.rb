cc5273c2bf3e1713b6b1e84569677fe86f31b9c5
izaxveta <izavetha@gmail.com>
2018-01-08 23:02:25 -0700

    Restaurant filter parses through all restaurant categories and returns only categories that are applicable to the US

--- app/models/venue_category.rb
+++ app/models/venue_category.rb
@@ -4,6 +4,12 @@ class VenueCategory < ApplicationRecord
     whitelisted_data = venue_categories_filter(categories_data).compact
   def self.venue_categories_filter(categories_data)
     categories_data.map do |category|
+      restaurant_filter(category) if category["parents"].include?('restaurants')
     end
+  end
+
+  def self.restaurant_filter(category)
+    return category if category["country_whitelist"] && category["country_whitelist"].include?("US")
+    return category if category["country_whitelist"].nil? && category["country_blacklist"].nil?
   end
 end
