8678f1fc2911451929a8d5fed7156e5e482bd0f5
izaxveta <izavetha@gmail.com>
2018-01-08 22:54:05 -0700

    Updated schema and created VenueCategories table

---
+++ db/migrate/20180109040900_create_venue_categories.rb
@@ -0,0 +1,8 @@
+class CreateVenueCategories < ActiveRecord::Migration[5.1]
+  def change
+    create_table :venue_categories do |t|
+      t.string :name
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180109024919) do
+ActiveRecord::Schema.define(version: 20180109040900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(version: 20180109024919) do
     t.string "nationality"
     t.string "occupation"
     t.integer "role", default: 0
+  end
+
+  create_table "venue_categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "user_brands", "brands"
