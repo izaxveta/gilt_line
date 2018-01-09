8e8384b98d50b95e5d9fa5116383735b935a23bd
izaxveta <izavetha@gmail.com>
2018-01-08 23:01:14 -0700

    #venue_categories_filter filters json input to only include venues that are restaruants and then sends it through a restaurant filter

--- app/models/venue_category.rb
+++ app/models/venue_category.rb
@@ -1,4 +1,9 @@
 class VenueCategory < ApplicationRecord
   validates :name, presence: true
 
+    whitelisted_data = venue_categories_filter(categories_data).compact
+  def self.venue_categories_filter(categories_data)
+    categories_data.map do |category|
+    end
+  end
 end
