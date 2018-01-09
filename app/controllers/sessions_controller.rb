627633ddea4b538ef4439842199833ec315bf948
izaxveta <izavetha@gmail.com>
2018-01-08 17:12:04 -0700

    Handled dashboard redirect depending on user role, refactored redirect and render flash into separate methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:success] = 'Sign in successful!'
-      redirect_to dashboard_path(id: user.id)
+      render_flash
+      redirect_user(user)
     else
       render :new
     end
@@ -16,4 +16,15 @@ class SessionsController < ApplicationController
     session.clear
     redirect_to root_path
   end
+
+  private
+
+    def redirect_user(user)
+      redirect_to admin_dashboard_path if user.admin?
+      redirect_to dashboard_path(id: user.id) if user.default?
+    end
+
+    def render_flash
+      flash[:success] = 'Sign in successful!'
+    end
 end
