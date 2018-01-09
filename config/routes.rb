db960fdb61bbb1b35c4ffbb5445990b54c4f9ae5
izaxveta <izavetha@gmail.com>
2018-01-08 20:05:40 -0700

    Admin navigates to /settings and can view a link to manage brands, which initializes FashionBrands#index controller action

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   post '/login',         to: 'sessions#create'
   delete '/login',       to: 'sessions#destroy', as: 'logout'
   get 'admin/settings',  to: 'admin/dashboard#show', as: 'settings'
+
+  get '/admin/fashion-brands', to: 'admin/fashion_brands#index', as: 'fashion_brands'
   get '/profile',        to: 'users#show',       as: 'profile'
   get '/profile/edit',   to: 'users#edit',       as: 'edit_profile'
 
