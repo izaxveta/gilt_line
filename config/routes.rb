5a9bb0684514c381c4b91bc943e89ec50a7854ae
izaxveta <izavetha@gmail.com>
2018-01-08 17:11:19 -0700

    Namespaced admin dashboard from default user dashboard routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  get '/dashboard', to: 'dashboard#index'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/login', to: 'sessions#destroy', as: 'logout'
-  get '/profile', to: 'users#show', as: 'profile'
-  get '/profile/edit', to: 'users#edit', as: 'edit_profile'
+
+  get 'admin/dashboard', to: 'admin/dashboard#index'
+  get '/dashboard',      to: 'dashboard#index'
+  get '/login',          to: 'sessions#new'
+  post '/login',         to: 'sessions#create'
+  delete '/login',       to: 'sessions#destroy', as: 'logout'
+  get '/profile',        to: 'users#show',       as: 'profile'
+  get '/profile/edit',   to: 'users#edit',       as: 'edit_profile'
+
   resources :users, only: [:create, :update]
   resources :search, only: [:index]
 end
