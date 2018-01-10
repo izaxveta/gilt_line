9adc18e1e658e02a0693674221ff4f5d2ea5b186
izaxveta <izavetha@gmail.com>
2018-01-10 14:19:02 -0700

    Rendered show venues_controller#show action, and private method which sorts results by which tab a user is viewing.

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -3,5 +3,15 @@ class VenuesController < ApplicationController
 
   def show
     @venue = Venue.new(params[:venue])
+    @results ||= GiltGenerator.venue_suggestions(@venue.categories)
+    @items = sort_items(@results, params)
+  end
+
+  private
+
+  def sort_items(results, filter)
+    return GiltGenerator.sort_shoes(results) if filter[:shoes] && !results.nil?
+    return GiltGenerator.sort_accessories(results) if filter[:accessories] && !results.nil?
+    return GiltGenerator.sort_apparel(results) if !results.nil?
   end
 end
