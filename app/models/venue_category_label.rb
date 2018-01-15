118046a8af60e1bcfdef55fd660cfada57ac1629
izaxveta <izavetha@gmail.com>
2018-01-15 15:21:33 -0700

    Added ability to reset venue category labels before adding.

--- app/models/venue_category_label.rb
+++ app/models/venue_category_label.rb
@@ -3,6 +3,7 @@ class VenueCategoryLabel < ApplicationRecord
   belongs_to :label
 
   def self.set_venue_category_labels(venue_category, label_ids)
+    venue_category.venue_category_labels.delete_all
     label_ids.each { |id| venue_category.labels << Label.find(id) if id != '' }
   end
 end
