a6da6bd24f3a832789b6baa784c9e9594d2eeda8
izaxveta <izavetha@gmail.com>
2018-01-14 18:56:01 -0700

    Changed some methods from !.empty? to .any? Also, modified how generator was parsing thorugh data.

--- app/services/gilt_generator.rb
+++ app/services/gilt_generator.rb
@@ -2,8 +2,8 @@ class GiltGenerator
 
   def self.venue_suggestions(categories)
     venues = categories.map {|category| VenueCategory.find_or_create_by(name: category)}
-    venues_with_labels = venues.select { |venue| !venue.labels.empty? }
-    gilt_generator_sequence(venues_with_labels) if !venues_with_labels.empty?
+    venues_with_labels = venues.select { |venue| venue.labels.any? }
+    gilt_generator_sequence(venues_with_labels) if venues_with_labels.any?
   end
 
   def self.sort_apparel(collection)
@@ -23,7 +23,7 @@ class GiltGenerator
     def self.gilt_generator_sequence(venues)
       labels = fetch_labels(venues)
       query = formulate_query(labels)
-      raw_data = get_items_from_service(query)[:products]
+      raw_data = get_items_from_service(query)
       generate_items(raw_data)
     end
 
@@ -31,16 +31,21 @@ class GiltGenerator
       venues.map {|venue| venue.labels }.flatten.uniq
     end
 
+    def self.filter_products(items_data)
+     items_data.map { |data| data[:products] }.flatten
+    end
+
     def self.generate_items(items_data)
-      items_data.map { |item| Item.new(item) }
+      filtered_data = filter_products(items_data)
+      filtered_data.map { |item| Item.new(item) }
     end
 
     def self.formulate_query(labels)
-      labels.map {|label| "&brand=#{label.name}"}.join
+      labels.map {|label| "&brand=#{label.name}"}
     end
 
-    def self.get_items_from_service(query)
-      @fashion_items_data ||= gilt_service.get_items(query)
+    def self.get_items_from_service(queries)
+      queries.map { |query| gilt_service.get_items(query) }
     end
 
     def self.gilt_service
