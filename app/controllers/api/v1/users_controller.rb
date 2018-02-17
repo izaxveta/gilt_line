f0ad928225d675ca70f1bcce2b188236f88966e6
izaxveta <izavetha@gmail.com>
2018-02-17 14:46:59 -0700

    Build user controller for POST API

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save
+      render status: 201, json: {
+        message: "Successfully created #{user.full_name}",
+        user: user
+      }
+    else
+      render status: 400, json: {
+        message: "Invalid request"
+      }
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
+end
