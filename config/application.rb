3446a07bd85eff6ec3471fa82054ae1606b63533
izaxveta <izavetha@gmail.com>
2018-02-19 21:01:36 -0700

    Add config setting to include lib directory when application loads

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module GiltLine
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # JWT setting to include lib directory when application loads
+    config.autoload_paths << Rails.root.join('lib')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
