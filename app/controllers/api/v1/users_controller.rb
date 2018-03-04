d1fee8cfa7b87b3edbe43964f7c7908331669d65
izaxveta <izavetha@gmail.com>
2018-03-04 16:15:36 -0700

    Update users user_params private controller action for users api routes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -41,6 +41,14 @@ class Api::V1::UsersController < Api::V1::ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password)
+      params.require(:user).permit(:first_name,
+                                   :last_name,
+                                   :email,
+                                   :password,
+                                   :birthday,
+                                   :nationality,
+                                   :occupation,
+                                   :avatar,
+                                   :moniker)
     end
 end
