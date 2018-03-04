3649bd848bc322ec1f429508744df1fda1e1e3b8
izaxveta <izavetha@gmail.com>
2018-03-04 16:11:59 -0700

    Comment out authenticate_request before action to enable manual testing of api routes

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ApplicationController < ActionController::API
-  before_action :authenticate_request
+  # before_action :authenticate_request
     attr_reader :current_user
 
     private
