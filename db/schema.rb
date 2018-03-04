a16da5fb096d66395dad8db32d171618d2242c1c
izaxveta <izavetha@gmail.com>
2018-03-04 16:20:26 -0700

    Add moniker column to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180115021920) do
+ActiveRecord::Schema.define(version: 20180304215929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20180115021920) do
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string "moniker"
   end
 
   create_table "venue_categories", force: :cascade do |t|
