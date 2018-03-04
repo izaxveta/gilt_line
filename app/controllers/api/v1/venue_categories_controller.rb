c7eba34518badd17eef1439c883cff24e64adb6f
izaxveta <izavetha@gmail.com>
2018-03-04 16:16:51 -0700

    Built out VenueCategories show controller action for venue-categories api routes

--- app/controllers/api/v1/venue_categories_controller.rb
+++ app/controllers/api/v1/venue_categories_controller.rb
@@ -3,4 +3,11 @@ class Api::V1::VenueCategoriesController < Api::V1::ApplicationController
     render status: 200, json: VenueCategory.all
   end
 
+  def show
+    venue_category = VenueCategory.find_by(name: params[:name].capitalize)
+    render status: 200, json: {
+      venue_category: venue_category,
+      labels: venue_category.labels
+    }
+  end
 end
