4ea1f65084feda14d61b4abb0cafffbe69c699e7
izaxveta <izavetha@gmail.com>
2018-03-04 16:13:03 -0700

    Built out labels index controller action for labels api routes

--- app/controllers/api/v1/labels_controller.rb
+++ app/controllers/api/v1/labels_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::LabelsController < Api::V1::ApplicationController
+  def index
+    render status: 200, json: Label.all
+  end
+
   def show
     label = Label.find_by(name: params[:name])
     render status: 200, json: label
