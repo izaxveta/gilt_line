cdfdeeb7b4301d1d20752da2314060aa2ec33db4
izaxveta <izavetha@gmail.com>
2018-03-05 18:25:44 -0700

    Modify AuthenticateUser class to initialize with moniker, passing in moniker from params

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::AuthenticationController < Api::V1::ApplicationController
   skip_before_action :authenticate_request
 
   def authenticate
-    command = AuthenticateUser.call(params[:email], params[:password])
+    command = AuthenticateUser.call(params[:moniker], params[:password])
 
     if command.success?
       render json: { auth_token: command.result }
