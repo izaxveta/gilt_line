4b11e2bc4e6fc155fd5e0aa482c6563e83548e1d
izaxveta <izavetha@gmail.com>
2018-03-09 14:42:18 -0700

    Clean up spacing and order of routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,18 @@ Rails.application.routes.draw do
   post '/api/v1/authenticate', to: 'api/v1/authentication#authenticate'
 
   # USERS API
-  get '/api/v1/users', to: 'api/v1/users#index'
+  get '/api/v1/users',          to: 'api/v1/users#index'
   get '/api/v1/users/:moniker', to: 'api/v1/users#show'
-  post '/api/v1/users', to: 'api/v1/users#create'
+  post '/api/v1/users',         to: 'api/v1/users#create'
   put '/api/v1/users/:moniker', to: 'api/v1/users#update'
 
   # LABELS API
-  get '/api/v1/labels', to: 'api/v1/labels#index'
+  get '/api/v1/labels',       to: 'api/v1/labels#index'
   get '/api/v1/labels/:name', to: 'api/v1/labels#show'
-  post '/api/v1/labels', to: 'api/v1/labels#create'
+  post '/api/v1/labels',      to: 'api/v1/labels#create'
 
   # VENUE CATEGORIES API
-  get '/api/v1/venue-categories', to: 'api/v1/venue_categories#index'
+  get '/api/v1/venue-categories',       to: 'api/v1/venue_categories#index'
   get '/api/v1/venue-categories/:name', to: 'api/v1/venue_categories#show'
 
   #VENUES API
@@ -38,20 +38,21 @@ Rails.application.routes.draw do
   get '/admin/labels',     to: 'admin/labels#index', as: 'labels'
   get '/admin/labels/new', to: 'admin/labels#new',   as: 'new_label'
   post '/admin/labels',    to: 'admin/labels#create'
-  patch '/admin/',    to: 'admin/labels#create'
+  patch '/admin/',         to: 'admin/labels#create'
 
   get '/admin/venue-categories',           to: 'admin/venue_categories#index', as: 'venue_categories'
   get '/admin/venue-categories/generator', to: 'admin/venue_categories#new',   as: 'venue_category_generator'
   get '/admin/venue-categories/:id',       to: 'admin/venue_categories#show',  as: 'venue_category'
+  get '/admin/venue-categories/:id/edit',  to: 'admin/venue_categories#edit',  as: 'edit_venue_category'
   post '/admin/venue-categories',          to: 'admin/venue_categories#create'
-  get '/admin/venue-categories/:id/edit',       to: 'admin/venue_categories#edit', as: 'edit_venue_category'
-  patch '/admin/venue-categories/:id',       to: 'admin/venue_categories#update'
+  patch '/admin/venue-categories/:id',     to: 'admin/venue_categories#update'
 
   get '/dashboard',      to: 'dashboard#index'
 
-  get '/profile',        to: 'users#show',       as: 'profile'
-  get '/profile/edit',   to: 'users#edit',       as: 'edit_profile'
+  get '/profile',      to: 'users#show', as: 'profile'
+  get '/profile/edit', to: 'users#edit', as: 'edit_profile'
 
   get '/search', to: 'search#index', as: 'search_index'
+
   resources :users, only: [:create, :update]
 end
