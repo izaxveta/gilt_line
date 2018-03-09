ecb32e22f5e4575d08fa589ed02f2c137e627f78
izaxveta <izavetha@gmail.com>
2018-03-09 15:01:40 -0700

    Create index action for Api V1 ItemsController, returns json of item count and items related to categories query passed in

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,2 +1,10 @@
 class Api::V1::ItemsController < Api::V1::ApplicationController
+  def index
+    categories = params[:q].split(',')
+    items = GiltGenerator.venue_suggestions(categories)
+    render status: 200, json: {
+      item_count: items.count,
+      items: items
+    }
+  end
 end
